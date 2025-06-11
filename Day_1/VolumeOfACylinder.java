6e87724dd6396ad3615ad6fce103e8f990d5af5c
Abhishek Sharma <122424312+abhisheksharma8395@users.noreply.github.com>
2025-06-11 12:24:36 +0530

    Update and rename program5.java to VolumeOfACylinder.java

--- Day_1/VolumeOfACylinder.java
+++ Day_1/VolumeOfACylinder.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class program5 {
+class VolumeOfACylinder {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter radius of the cylinder:");
