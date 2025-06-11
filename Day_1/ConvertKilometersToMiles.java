ac43e0b2fb12829304b7ad6c8f500290c154a191
Abhishek Sharma <122424312+abhisheksharma8395@users.noreply.github.com>
2025-06-11 12:31:24 +0530

    Update and rename program10.java to ConvertKilometersToMiles.java

--- Day_1/ConvertKilometersToMiles.java
+++ Day_1/ConvertKilometersToMiles.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
-class program10 {
+class ConvertKilometersToMiles {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-		System.out.println("Enter distance in kilometers:");
+	System.out.println("Enter distance in kilometers:");
         double km = sc.nextDouble();
-		double m = km * 0.621371;
+	double m = km * 0.621371;
         System.out.println(km + " kilometers is equal to " + m + " miles.");
     }
 }
