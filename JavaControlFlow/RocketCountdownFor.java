245b94f7b412549222d6c6b6a202fd6fa7996c38
Abhishek Sharma <abhiparashar8630@gmail.com>
2025-06-13 20:58:39 +0530

    Added: Program to build Counter from User Input to 1 for rocket launch

---
+++ JavaControlFlow/RocketCountdownFor.java
@@ -0,0 +1,21 @@
+// RocketCountdownFor.java
+
+import java.util.Scanner;
+
+public class RocketCountdownFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Take countdown start value from user
+        System.out.print("Enter countdown start number: ");
+        int counter = sc.nextInt();
+
+        // Loop from counter down to 1
+        for (int i = counter; i >= 1; i--) {
+            System.out.println(i);
+        }
+
+        System.out.println("Rocket Launched!");
+        sc.close();
+    }
+}
