86c598eb03e79ca5b9452e241ddce516f99f4515
Abhishek Sharma <abhiparashar8630@gmail.com>
2025-06-13 20:45:34 +0530

    Added: Program to check if the number is divisible by 5 or not

---
+++ JavaControlFlow/DivisibilityChecker.java
@@ -0,0 +1,22 @@
+// DivisibilityChecker.java
+
+import java.util.Scanner;
+
+public class DivisibilityChecker {
+    public static void main(String[] args) {
+        // Create a Scanner object to take user input
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        // Check if the number is divisible by 5
+        if (number % 5 == 0) {
+            System.out.println("The number " + number + " is divisible by 5.");
+        } else {
+            System.out.println("The number " + number + " is not divisible by 5.");
+        }
+
+        sc.close();
+    }
+}
