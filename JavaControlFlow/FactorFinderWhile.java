424fa8a1da86d727b86bff730015f450a1dc44aa
Abhishek Sharma <abhiparashar8630@gmail.com>
2025-06-14 01:07:38 +0530

    Added: Program to calculate all factor of a number using while loop

---
+++ JavaControlFlow/FactorFinderWhile.java
@@ -0,0 +1,28 @@
+// FactorFinderWhile.java
+
+import java.util.Scanner;
+
+public class FactorFinderWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input a positive number
+        System.out.print("Enter a positive number: ");
+        int number = sc.nextInt();
+
+        if (number > 0) {
+            int counter = 1;
+            System.out.println("Factors of " + number + " are:");
+            while (counter <= number) {
+                if (number % counter == 0) {
+                    System.out.println(counter);
+                }
+                counter++;
+            }
+        } else {
+            System.out.println("Please enter a positive integer.");
+        }
+
+        sc.close();
+    }
+}
