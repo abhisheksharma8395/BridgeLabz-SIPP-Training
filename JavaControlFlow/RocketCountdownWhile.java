cbf6dfd35dee7a4d087bdd92915a83c1b8516ea1
Abhishek Sharma <abhiparashar8630@gmail.com>
2025-06-14 00:47:09 +0530

    Added: Program to create a counter using while loop for a rocket launch

---
+++ JavaControlFlow/RocketCountdownWhile.java
@@ -0,0 +1,22 @@
+// RocketCountdownWhile.java
+
+import java.util.Scanner;
+
+public class RocketCountdownWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input starting number for countdown
+        System.out.print("Enter starting countdown number: ");
+        int counter = sc.nextInt();
+
+        // Countdown using while loop
+        while (counter >= 1) {
+            System.out.println(counter);
+            counter--;
+        }
+
+        System.out.println("Rocket Launched!");
+        sc.close();
+    }
+}
