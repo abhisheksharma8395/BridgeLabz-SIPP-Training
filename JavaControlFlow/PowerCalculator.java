f4cffd01cae6ea5de051773b1894a542748b6ba6
Abhishek Sharma <abhiparashar8630@gmail.com>
2025-06-14 01:01:14 +0530

    Added: Program to calculate the power of a number

---
+++ JavaControlFlow/PowerCalculator.java
@@ -0,0 +1,27 @@
+// PowerCalculator.java
+
+import java.util.Scanner;
+
+public class PowerCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input base and exponent
+        System.out.print("Enter the base number: ");
+        int number = sc.nextInt();
+
+        System.out.print("Enter the power: ");
+        int power = sc.nextInt();
+
+        int result = 1;
+
+        // Calculate power using for loop
+        for (int i = 1; i <= power; i++) {
+            result *= number;
+        }
+
+        System.out.println(number + " raised to the power " + power + " is " + result);
+
+        sc.close();
+    }
+}
