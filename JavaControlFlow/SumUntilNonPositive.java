f81e55c281adce344444fe13a71b22aebe254959
Abhishek Sharma <abhiparashar8630@gmail.com>
2025-06-14 00:41:31 +0530

    Added: Program to calculate the sum of user input values until user enters 0 or a negative numbers

---
+++ JavaControlFlow/SumUntilNonPositive.java
@@ -0,0 +1,25 @@
+// SumUntilNonPositive.java
+
+import java.util.Scanner;
+
+public class SumUntilNonPositive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double total = 0.0;
+
+        // Loop until user enters 0 or negative number
+        while (true) {
+            System.out.print("Enter a number (0 or negative to stop): ");
+            double value = sc.nextDouble();
+
+            if (value <= 0) {
+                break;
+            }
+
+            total += value;
+        }
+
+        System.out.println("Total sum is: " + total);
+        sc.close();
+    }
+}
