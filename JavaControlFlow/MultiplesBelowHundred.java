4e2c47e5d4ccd07157c95d61a2e5f720e6e421fe
Abhishek Sharma <abhiparashar8630@gmail.com>
2025-06-14 00:55:40 +0530

    Added: Program to calculate all the multiples of a number below 100

---
+++ JavaControlFlow/MultiplesBelowHundred.java
@@ -0,0 +1,28 @@
+// MultiplesBelowHundred.java
+
+import java.util.Scanner;
+
+public class MultiplesBelowHundred {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input number
+        System.out.print("Enter a number less than 100: ");
+        int number = sc.nextInt();
+
+        if (number > 0 && number < 100) {
+            int counter = number - 1;
+            System.out.println("Multiples of " + number + " below 100:");
+            while (counter > 1) {
+                if (counter % number == 0) {
+                    System.out.println(counter);
+                }
+                counter--;
+            }
+        } else {
+            System.out.println("Number must be a positive integer less than 100.");
+        }
+
+        sc.close();
+    }
+}
