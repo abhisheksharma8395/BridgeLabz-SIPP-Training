b21795ab49e9a68d994787e748b9e8a76ca0ce22
Abhishek Sharma <abhiparashar8630@gmail.com>
2025-06-14 02:13:57 +0530

    Added: Program to find all multiples of a number below 100 using backward loop

---
+++ JavaControlFlow/MultiplesBelowHundredReverse.java
@@ -0,0 +1,26 @@
+// MultiplesBelowHundredReverse.java
+
+import java.util.Scanner;
+
+public class MultiplesBelowHundredReverse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input number
+        System.out.print("Enter a number less than 100: ");
+        int number = sc.nextInt();
+
+        if (number > 0 && number < 100) {
+            System.out.println("Multiples of " + number + " below 100 (in reverse):");
+            for (int i = 100; i >= 1; i--) {
+                if (i % number == 0) {
+                    System.out.println(i);
+                }
+            }
+        } else {
+            System.out.println("Enter a number between 1 and 99.");
+        }
+
+        sc.close();
+    }
+}
