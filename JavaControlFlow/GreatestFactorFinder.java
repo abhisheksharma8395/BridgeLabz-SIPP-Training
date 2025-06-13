8603903170a7d2794419d9c09cdd541ffcab2c47
Abhishek Sharma <abhiparashar8630@gmail.com>
2025-06-14 01:04:53 +0530

    Added: Program to calculate greatest factor of a number excluding itself using while loop

---
+++ JavaControlFlow/GreatestFactorFinder.java
@@ -0,0 +1,28 @@
+// GreatestFactorFinder.java
+
+import java.util.Scanner;
+
+public class GreatestFactorFinder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input number
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int greatestFactor = 1;
+        int counter = number - 1;
+
+        // Loop from number-1 to 1
+        while (counter >= 1) {
+            if (number % counter == 0) {
+                greatestFactor = counter;
+                break;
+            }
+            counter--;
+        }
+
+        System.out.println("The greatest factor of " + number + " besides itself is: " + greatestFactor);
+        sc.close();
+    }
+}
