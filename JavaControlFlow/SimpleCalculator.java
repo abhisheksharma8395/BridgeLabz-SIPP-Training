7e2bff17451f20572755754fbb2616109d54edfe
Abhishek Sharma <abhiparashar8630@gmail.com>
2025-06-14 15:36:45 +0530

    Added: program to build a simple calculator which performs addition subtraction multiplication and division on the basis of user input

---
+++ JavaControlFlow/SimpleCalculator.java
@@ -0,0 +1,42 @@
+// SimpleCalculator.java
+
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input two numbers and an operator
+        System.out.print("Enter first number: ");
+        double first = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double second = sc.nextDouble();
+
+        System.out.print("Enter operator (+, -, *, /): ");
+        String op = sc.next();
+
+        // Perform operation based on operator
+        switch (op) {
+            case "+":
+                System.out.println("Result: " + (first + second));
+                break;
+            case "-":
+                System.out.println("Result: " + (first - second));
+                break;
+            case "*":
+                System.out.println("Result: " + (first * second));
+                break;
+            case "/":
+                if (second != 0)
+                    System.out.println("Result: " + (first / second));
+                else
+                    System.out.println("Cannot divide by zero.");
+                break;
+            default:
+                System.out.println("Invalid Operator.");
+        }
+
+        sc.close();
+    }
+}
