a6506ec82b8527bdfebf3cbf6666c2b5c322e049
Abhishek Sharma <abhiparashar8630@gmail.com>
2025-06-14 15:19:19 +0530

    Added: program to calculate greatest factor of a number using for loop

---
+++ JavaControlFlow/GreatestFactorUsingFor.java
@@ -0,0 +1,26 @@
+// GreatestFactorUsingFor.java
+
+import java.util.Scanner;
+
+public class GreatestFactorUsingFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input integer
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int greatestFactor = 1;
+
+        // Find greatest factor (excluding number itself)
+        for (int i = number - 1; i >= 1; i--) {
+            if (number % i == 0) {
+                greatestFactor = i;
+                break;
+            }
+        }
+
+        System.out.println("The greatest factor of " + number + " besides itself is: " + greatestFactor);
+        sc.close();
+    }
+}
