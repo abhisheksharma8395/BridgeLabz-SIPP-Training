aca01bf6ce9432c163b0aa712f9f9b67c1d40199
Abhishek Sharma <abhiparashar8630@gmail.com>
2025-06-14 15:21:41 +0530

    Added: program to calculate power of a number using while loop

---
+++ JavaControlFlow/PowerCalculatorWhile.java
@@ -0,0 +1,29 @@
+// PowerCalculatorWhile.java
+
+import java.util.Scanner;
+
+public class PowerCalculatorWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input number and power
+        System.out.print("Enter base number: ");
+        int number = sc.nextInt();
+
+        System.out.print("Enter power: ");
+        int power = sc.nextInt();
+
+        int result = 1;
+        int counter = 0;
+
+        // Calculate power using while loop
+        while (counter < power) {
+            result *= number;
+            counter++;
+        }
+
+        System.out.println(number + " raised to the power " + power + " is: " + result);
+
+        sc.close();
+    }
+}
