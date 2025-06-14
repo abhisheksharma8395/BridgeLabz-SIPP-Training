bc2408c5ab1a8fd244baa0b3ac24b86b1052cfd3
Abhishek Sharma <abhiparashar8630@gmail.com>
2025-06-14 16:00:26 +0530

    Added: program to check wheather a input year is a leap year or not

---
+++ JavaControlFlow/LeapYearCheckerSecond.java
@@ -0,0 +1,22 @@
+// LeapYearCheckerSecond.java
+
+import java.util.Scanner;
+
+public class LeapYearCheckerSecond {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input year
+        System.out.print("Enter a year: ");
+        int year = sc.nextInt();
+
+        // Check valid Gregorian calendar
+        if (year >= 1582 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)) {
+            System.out.println(year + " is a Leap Year.");
+        } else {
+            System.out.println(year + " is Not a Leap Year.");
+        }
+
+        sc.close();
+    }
+}
