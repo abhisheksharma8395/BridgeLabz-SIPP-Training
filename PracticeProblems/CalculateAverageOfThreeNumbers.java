bc3542954ccf443e1426210038ba438a302967ed
Abhishek Sharma <abhiparashar8630@gmail.com>
2025-06-16 23:04:10 +0530

    Refactor:a program to calculate the average of three numbers.

--- PracticeProblems/CalculateAverageOfThreeNumbers.java
+++ PracticeProblems/CalculateAverageOfThreeNumbers.java
@@ -1,14 +1,24 @@
+package PracticeProblems;
+
 import java.util.Scanner;
 class CalculateAverageOfThreeNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-	System.out.println("Enter first number:");
+
+        // taking input for first number
+        System.out.println("Enter first number:");
         double a = sc.nextDouble();
-	System.out.println("Enter second number:");
+
+        // taking input for second number
+        System.out.println("Enter second number:");
         double b = sc.nextDouble();
-	System.out.println("Enter third number:");
+
+        // taking input for third number
+	    System.out.println("Enter third number:");
         double c = sc.nextDouble();
-	double average = (a + b + c) / 3;
+
+        //calculating result
+	    double average = (a + b + c) / 3;
         System.out.println("Average of the three numbers is: " + average);
     }
 }
