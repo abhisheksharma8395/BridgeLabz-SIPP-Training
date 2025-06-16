9654b3415fb87d069701e828dbbf3f74cf9eddc4
Abhishek Sharma <abhiparashar8630@gmail.com>
2025-06-16 23:01:02 +0530

    Refactor:a program to covert celsius degree temperature to fahrenheit degree.

--- PracticeProblems/CelsiusToFahrenheitConversion.java
+++ PracticeProblems/CelsiusToFahrenheitConversion.java
@@ -1,9 +1,12 @@
+package PracticeProblems;
+
 import java.util.Scanner;
 class CelsiusToFahrenheitConversion{
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter temperature in celsius degree :");
-        int a = sc.nextInt();
-	int c = (a*(9/5))+32;
-	System.out.println("Temperature is "+c+" Fahrenheit degree");
+        int celisusDegree = sc.nextInt();
+        int FahrenheitDegree = (celisusDegree*(9/5))+32;
+        System.out.println("Temperature is "+celisusDegree+" Fahrenheit degree");
     }
 }
