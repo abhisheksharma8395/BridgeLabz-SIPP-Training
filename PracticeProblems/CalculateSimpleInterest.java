1113789778573838b9b76c2b9366a10fd4f6857a
Abhishek Sharma <abhiparashar8630@gmail.com>
2025-06-16 22:59:21 +0530

    Refactor:a program to calculate Simple Interest on amount at rate r for time t.

--- PracticeProblems/CalculateSimpleInterest.java
+++ PracticeProblems/CalculateSimpleInterest.java
@@ -1,13 +1,15 @@
+package PracticeProblems;
+
 import java.util.Scanner;
 class CalculateSimpleInterest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Principal:");
-        double p = sc.nextDouble();
+        double principal = sc.nextDouble();
         System.out.println("Enter Rate of Interest:");
-        double r = sc.nextDouble();
+        double rate = sc.nextDouble();
         System.out.println("Enter Time (in years):");
-        double t = sc.nextDouble();
+        double time = sc.nextDouble();
         double interest = (principal * rate * time) / 100;
         System.out.println("Simple Interest is: " + interest);
     }
