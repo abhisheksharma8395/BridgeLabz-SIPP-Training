99709a43c4faf0b77727d27cd6c59e590859e930
Abhishek Sharma <abhiparashar8630@gmail.com>
2025-06-16 23:04:51 +0530

    Refactor:a program to calculate the addition of two numbers.

--- PracticeProblems/AddTwoNumbers.java
+++ PracticeProblems/AddTwoNumbers.java
@@ -1,11 +1,19 @@
+package PracticeProblems;
+
 import java.util.Scanner;
 class AddTwoNumbers{
     public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-                System.out.println("Enter First Number");
+
+		//Taking Input for first number
+		System.out.println("Enter First Number");
 		int a = sc.nextInt();
+
+		//Taking Input for first number
 		System.out.println("Enter Second Number");
 		int b = sc.nextInt();
+		
+		//Calculating results
 		int c = a+b;
 		System.out.println("Addition of "+a+" And "+b+" is :"+c);
     }
