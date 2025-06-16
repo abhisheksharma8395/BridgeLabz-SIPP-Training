672cf5bf1039d7a29acf2f712736a12435314e9e
Abhishek Sharma <abhiparashar8630@gmail.com>
2025-06-16 11:22:58 +0530

    Refactor:a program to first calculate average marks of three subjects and then on the basis of the average marks calculate grade and give remark.

--- JavaControlFlow/GradeCalculator.java
+++ JavaControlFlow/GradeCalculator.java
@@ -1,5 +1,5 @@
 // GradeCalculator.java
-
+package JavaControlFlow;
 import java.util.Scanner;
 
 public class GradeCalculator {
@@ -21,17 +21,17 @@ public class GradeCalculator {
 
         // Determine grade
         if (average >= 80) {
-            System.out.println("Average Marks is +"average" and Grade is: A and Remark is Level 4, above agency-nomalized standards");
+            System.out.println("Average Marks is +"+average+" and Grade is: A and Remark is Level 4, above agency-nomalized standards");
         } else if (average >= 70) {
-            System.out.println("Average Marks is +"average" and Grade is: B and Remark is Level 3, at agency-nomalized standards");
+            System.out.println("Average Marks is +"+average+" and Grade is: B and Remark is Level 3, at agency-nomalized standards");
         } else if (average >= 60) {
-            System.out.println("Average Marks is +"average" and Grade is: C and Remark is Level 2, but approaching agency-nomalized standards");
+            System.out.println("Average Marks is +"+average+" and Grade is: C and Remark is Level 2, but approaching agency-nomalized standards");
         } else if (average >= 50) {
-            System.out.println("Average Marks is +"average" and Grade is: D and Remark is Level 1, well below agency-nomalized standards");
+            System.out.println("Average Marks is +"+average+" and Grade is: D and Remark is Level 1, well below agency-nomalized standards");
         } else if (average >= 40) {
-            System.out.println("Average Marks is +"average" and Grade is: E and Remark is Level 1, too below agency-nomalized standards");
+            System.out.println("Average Marks is +"+average+" and Grade is: E and Remark is Level 1, too below agency-nomalized standards");
         } else {
-            System.out.println("Average Marks is +"average" and Grade is: R and Remark is Remedial standards");
+            System.out.println("Average Marks is +"+average+" and Grade is: R and Remark is Remedial standards");
         }
 
         sc.close();
