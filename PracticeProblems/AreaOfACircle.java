74f912cb8202bdb7cbf843be000fa0cff9adeb66
Abhishek Sharma <abhiparashar8630@gmail.com>
2025-06-16 23:04:31 +0530

    Refactor:a program to calculate the area of circle.

--- PracticeProblems/AreaOfACircle.java
+++ PracticeProblems/AreaOfACircle.java
@@ -1,10 +1,14 @@
+package PracticeProblems;
+import java.lang.Math;
 import java.util.Scanner;
 class AreaOfACircle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        //Taking input for radius of the circle
         System.out.println("Enter radius of the circle:");
-        double r = sc.nextDouble();
-        double a = Math.PI * radius * radius;
-        System.out.println("Area of the circle is: " + a);
+        double radius = sc.nextDouble();
+        //calculating area
+        double area = Math.PI * radius * radius;
+        System.out.println("Area of the circle is: " + area);
     }
 }
