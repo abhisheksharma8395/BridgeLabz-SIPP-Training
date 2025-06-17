8485ba996ae3bfe730f82472cd052d6e88d2cc00
Abhishek Sharma <abhiparashar8630@gmail.com>
2025-06-17 09:27:44 +0530

    Refactor:a program to take input of 10 ages and check on each age for casting vote.

---
+++ Arraypractice/CanCastVote.java
@@ -0,0 +1,30 @@
+package Arraypractice;
+
+import java.util.Scanner;
+
+public class CanCastVote {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Array for storing 10 persons age
+        int[] ages = new int[10];
+
+        //Taking input for 10 ages
+        for(int i = 0; i < 10; i++){
+            ages[i] = sc.nextInt();
+        }
+
+        //Looping through array ages and prints the output
+        for(int i = 0;i <= ages.length;i++){
+            if(ages[i] < 0){
+                System.out.println("An Invalid age!");
+            }
+            else if(ages[i] == 0){
+                System.out.println("The student with the age "+ages[i]+" can vote");
+            }
+            else{
+                System.out.println("The student with the age "+ages[i]+" cannot vote");
+            }
+        }
+
+    }
+}
