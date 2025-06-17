a5845df2a600f0dbbae121403f0b539ef901366d
Abhishek Sharma <abhiparashar8630@gmail.com>
2025-06-17 08:49:09 +0530

    Refactor:a program to calculate the print the statement Welcome To BridgeLabz!

--- PracticeProblems/WelcomeToBridgeLabz.java
+++ PracticeProblems/WelcomeToBridgeLabz.java
@@ -1,5 +1,8 @@
+package PracticeProblems;
+
 class WelcomeToBridgeLabz{
     public static void main(String[] args){
+        //Printing the statement
         System.out.println("Welcome to Bridgelabz!");
     }
 }
